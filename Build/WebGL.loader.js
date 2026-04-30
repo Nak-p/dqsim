649f04d45589e1fe1c750c4902e058ddc7ad5073
Nak-p <nakazawa@pragmaadvisory.net>
2026-05-01 00:03:59 +0900

    Deploy WebGL build - 2026-05-01 00:03

--- Build/WebGL.loader.js
+++ Build/WebGL.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 58344534,
+    wasmFileSize: 58349193,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
